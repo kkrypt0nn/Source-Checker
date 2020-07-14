09bd170dbcd82ade7dcbf7b7ad21cf26b5dc5c7c
Krypton <43011723+kkrypt0nn@users.noreply.github.com>
2020-07-14 20:41:26 +0200

    Added gitinore and created project under VisualStudio

---
+++ Source-Checker/Source-Checker/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Source_Checker
+{
+    public partial class Source_Checker : Form
+    {
+        public Source_Checker()
+        {
+            InitializeComponent();
+        }
+    }
+}
